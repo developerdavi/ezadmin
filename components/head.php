8a92444d906e59497aa0945fd9716dfab033ac0c
Davi Rodrigues <developer.davi@gmail.com>
2020-03-11 22:10:26 -0300

    Imported custom CSS

--- components/head.php
+++ components/head.php
@@ -3,6 +3,7 @@
 <head>
 	<title>Ez SQL Admin</title>
 	<link rel="stylesheet" type="text/css" href="css/materialize.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
   <link href="css/material-icons.css" rel="stylesheet">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <style type="text/css">
